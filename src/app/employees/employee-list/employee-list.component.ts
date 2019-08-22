64e4827e496c47494656c22112da0a66a008b6c4
Houssnou <hosni.moubarak@gmail.com>
2019-08-22 07:10:53 -0400

    Fully Implemented DataTables Sorting, Paging and Search

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { EmployeeService } from 'src/app/shared/employee.service';
 import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
+import { DepartmentService } from 'src/app/shared/department.service';
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
@@ -8,10 +9,11 @@ import { MatTableDataSource, MatSort, MatPaginator } from '@angular/material';
 })
 export class EmployeeListComponent implements OnInit {
 
-  constructor(private service: EmployeeService) { }
+  constructor(private service: EmployeeService,
+              private departmentService: DepartmentService) { }
 
   dataSource: MatTableDataSource<any>;
-  displayedColumns: string[] = ['fullName', 'email', 'phoneNumber', 'city', 'actions'];
+  displayedColumns: string[] = ['fullName', 'email', 'phoneNumber', 'city', 'departmentName', 'actions'];
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   searchKey: string;
@@ -20,14 +22,22 @@ export class EmployeeListComponent implements OnInit {
     this.service.getEmployees().subscribe(
       list => {
         const employeeArray = list.map(item => {
+          // tslint:disable-next-line:prefer-const
+          let departmentName = this.departmentService.getDepartmentName(item.payload.val()['department']);
           return {
             $key: item.key,
+            departmentName,
             ...item.payload.val()
           };
         });
         this.dataSource = new MatTableDataSource(employeeArray);
         this.dataSource.sort = this.sort;
         this.dataSource.paginator = this.paginator;
+        this.dataSource.filterPredicate = (data, filter) => {
+            return this.displayedColumns.some(elt => {
+              return elt !== 'actions' && data[elt].toLowerCase().indexOf(filter) !== -1;
+            });
+        };
       });
   }
 
--- src/app/shared/department.service.ts
+++ src/app/shared/department.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import * as _ from 'lodash';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,8 @@ export class DepartmentService {
 
   constructor(private firebase: AngularFireDatabase) {
     this.departmentList = this.firebase.list('departments');
-    this.departmentList.snapshotChanges().subscribe(list => {
+    this.departmentList.snapshotChanges().subscribe(
+      list => {
       this.departmentArray = list.map(item => {
         return {
           $key: item.key,
@@ -18,5 +20,14 @@ export class DepartmentService {
         };
       });
     });
+  }
+
+  getDepartmentName($key: string) {
+    if ($key === '0') {
+      return '';
+    } else {
+      // tslint:disable-next-line:arrow-return-shorthand
+      return _.find(this.departmentArray, (obj) => { return obj.$key === $key; })['name'];
+    }
   }
 }
